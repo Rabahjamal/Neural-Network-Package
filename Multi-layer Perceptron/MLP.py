7c7714158bc92334e7a2634a57b0048ee93be5b0
Rabah Jamal <rabah9686@gmail.com>
2018-04-15 22:45:34 +0200

    Update MLP.py

--- Multi-layer Perceptron/MLP.py
+++ Multi-layer Perceptron/MLP.py
@@ -32,54 +32,6 @@ class MLP:
 
     def fit(self, X, y, epochs, learning_rate):
 
-        # for epoch in range(epochs):
-        #
-        #     # forward step
-        #     prv_a = X
-        #     for i in range(len(self.__layers)):
-        #         w = self.__layers[i][3]
-        #         z = self.__net_value(prv_a, w)
-        #         a = 0
-        #         if self.__layers[i][1] == 'sigmoid':
-        #             a = self.__sigmoid(z)
-        #         elif self.__layers[i][1] == 'tanh':
-        #             a = self.__tanh(z)
-        #         elif self.__layers[i][1] == 'relu':
-        #             a = self.__relu(z)
-        #         elif self.__layers[i][1] == 'softmax':
-        #             a = self.__softmax(z)
-        #         self.__layers[i][4] = a
-        #         prv_a = a
-        #
-        #     # backward step
-        #     self.__layers.reverse()
-        #     derv = np.multiply(self.__layers[0][4], (1 - self.__layers[0][4]))
-        #     prv_delta = np.multiply(y-self.__layers[0][4], derv)
-        #     prv_w = self.__layers[0][3]
-        #     self.__layers[0][5] = prv_delta
-        #     l = 1
-        #     for i in range(len(self.__layers[1:])):
-        #         a = self.__layers[l][4]
-        #         derv = np.multiply(a, (1-a))
-        #         delta = np.multiply(np.dot(prv_delta, np.transpose(prv_w[1:])), derv)
-        #         self.__layers[l][5] = delta
-        #         prv_delta = delta
-        #         prv_w = self.__layers[l][3]
-        #         l = l + 1
-        #
-        #     # weights update step
-        #     self.__layers.reverse()
-        #     prv_a = X
-        #     for i in range(len(self.__layers)):
-        #         w = self.__layers[i][3]
-        #         delta = self.__layers[i][5]
-        #         a0 = np.ones(shape=(1, X.shape[0]))
-        #         w[1:] = w[1:] + learning_rate * np.dot(np.transpose(prv_a), delta)
-        #         w[0] = w[0] + learning_rate * np.dot(a0, delta)
-        #         self.__layers[i][3] = w
-        #         prv_a = self.__layers[i][4]
-
-        # Unvectorized implementation
         for epoch in range(epochs):
             item = 0
             errors = []
